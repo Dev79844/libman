84d0ab7d3904524364189c84ea17b9f5a47d7ed2
ParikshitSaini2793 <parikshit.saini27@gmail.com>
2024-07-14 12:21:15 +0530

    add: AdminSide

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -4,9 +4,9 @@ import LoginForm from './Components/LoginForm';
 
 const LoginPage = () => {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white flex flex-col">
       <Navbar />
-      <div className="container mx-auto px-4 py-8 flex justify-center items-center">
+      <div className="flex-grow flex justify-center items-center">
         <LoginForm />
       </div>
     </div>
